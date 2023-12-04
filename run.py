712825527d645c9a25f0db2c39930cf650bc10e8
sabinakaczor <sabina.gbw@gmail.com>
2023-12-04 17:55:48 +0100

    Allow running only one part and measure time per part

--- run.py
+++ run.py
@@ -3,20 +3,34 @@ import inspect
 import sys
 import time
 from pathlib import Path
-
+        
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        duration = 1000 * (time.time() - start_time)
+        print('***** %s *****' % func.__name__.title().replace('_', ' '))
+        print(f'Result: {result}')
+        print(f'Duration: {duration} ms', end="\n\n")
+        
+    return wrapper
+        
 class Runner:
-    def __init__(self, day, test=False) -> None:
+    def __init__(self, day, test=False, part=None) -> None:
         self.year = '2023'
         self.day = day
         self.test = test
+        self.part = part
     
     def run(self):
         mod = importlib.import_module('{y}.{d}.solution'.format(y = self.year, d = self.day))
         solution = mod.Solution(self.test)
-        start_time = time.time()
-        solution.run()
-        duration = time.time() - start_time
-        print("--- %s seconds ---" % duration)
+        
+        if self.part is None or self.part == '1':
+            solution.part_one()
+        
+        if self.part is None or self.part == '2':
+            solution.part_two()
         
 class BaseSolution:
     def __init__(self, is_test) -> None:
@@ -28,9 +42,13 @@ class BaseSolution:
             
         return object.__getattribute__(self, name)
     
-    def run(self):
-        print('Part 1:', self.part1(), sep=' ')
-        print('Part 2:', self.part2(), sep=' ')
+    @timer
+    def part_one(self,):
+        return self.part1()
+    
+    @timer
+    def part_two(self,):
+        return self.part2()
             
     def set_path(self, part):
         self.input_filename = 'test{0}.txt'.format(part) if self.is_test else 'input.txt'
@@ -39,8 +57,15 @@ class BaseSolution:
         
 if __name__ == "__main__":
     day = sys.argv[1]
-    test = len(sys.argv) > 2 and sys.argv[2] == 'test'
-    runner = Runner(day, test)
+    test = 'test' in sys.argv
+    
+    part = None
+    for arg in sys.argv:
+        if arg.startswith('--part='):
+            part = arg.split('=')[-1]
+            break
+    
+    runner = Runner(day, test, part)
     runner.run()
         
     
